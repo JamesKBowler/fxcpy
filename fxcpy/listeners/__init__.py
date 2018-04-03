8f1cd02d1a0c4c9e95d836cb5f1c490b686b3f09
JamesKBowler <james.bowler@datacentauri.com>
2018-04-03 16:41:42 +0100

    Adding MIT License

--- fxcpy/listeners/__init__.py
+++ fxcpy/listeners/__init__.py
@@ -1,6 +1,4 @@
-
-
-from multiprocessing import Process, Value, Lock
+from multiprocessing import Value, Lock
 
 class Counter(object):
     def __init__(self, initval=0):
@@ -19,25 +17,3 @@ class Counter(object):
     def value(self):
         with self.lock:
             return self.val.value
-
-class EventHook(object):
-    def __init__(self):
-        self.__handlers = []
-
-    def __iadd__(self, handler):
-        self.__handlers.append(handler)
-        return self
-
-    def __isub__(self, handler):
-        self.__handlers.remove(handler)
-        return self
-
-    def fire(self, *args, **keywargs):
-        for handler in self.__handlers:
-            handler(*args, **keywargs)
-
-    def clearObjectHandlers(self, inObject):
-        self.__handlers = [h for h in self.__handlers if h.im_self != obj]
-        for theHandler in self.__handlers:
-            if theHandler.im_self == inObject:
-                self -= theHandler
