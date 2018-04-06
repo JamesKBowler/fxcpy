c725b0ec737ba89efda09ca669da3ebd481dbc35
JamesKBowler <james.bowler@datacentauri.com>
2018-04-06 20:02:12 +0100

    updating install script

--- install_script.sh
+++ install_script.sh
@@ -35,8 +35,9 @@ apt-get -y install libcurl4-openssl-dev
 apt-get -y install zlib1g-dev
 pip3 install ipython
 su $SUDO_USER << 'EOF'
-mkdir ~/.boost_install
+mkdir .boost_install
 EOF
+pwd
 #      ___ __                   ___        _______          ___
 #     / (_) /_  __  ___   __   <  /       <  / __ \        <  /
 #    / / / __ \/ / / / | / /   / /        / / /_/ /        / / 
@@ -44,47 +45,45 @@ EOF
 #  /_/_/_.___/\__,_/ |___/   /_/  (_)   /_//____/  (_)   /_/   
 #                                                              
 su $SUDO_USER << 'EOF'
-cd ~/.boost_install
+cd .boost_install
 wget https://github.com/libuv/libuv/archive/v1.19.1/libuv-1.19.1.tar.gz
 tar -xvzf libuv-1.19.1.tar.gz
-cd ~/.boost_install/libuv-1.19.1
+cd libuv-1.19.1
 sh autogen.sh  &&
 ./configure --prefix=/usr/local \
             --disable-static &&
 make
 EOF
-cd ~/.boost_install/libuv-1.19.1
+cd .boost_install/libuv-1.19.1
 make install -j2
-cd ~/.boost_install
 #      ___ __                    __    _               _____         _____         ___ 
 #     / (_) /_  ____ ___________/ /_  (_)   _____     |__  /        |__  /        |__ \
 #    / / / __ \/ __ `/ ___/ ___/ __ \/ / | / / _ \     /_ <          /_ <         __/ /
 #   / / / /_/ / /_/ / /  / /__/ / / / /| |/ /  __/   ___/ /  _     ___/ /  _     / __/ 
 #  /_/_/_.___/\__,_/_/   \___/_/ /_/_/ |___/\___/   /____/  (_)   /____/  (_)   /____/ 
-#                                                                                      
+#                     
+cd ..                           
 su $SUDO_USER << 'EOF'
-cd ~/.boost_install
 wget http://www.libarchive.org/downloads/libarchive-3.3.2.tar.gz
 tar -xvzf libarchive-3.3.2.tar.gz
-cd ~/.boost_install/libarchive-3.3.2
+cd libarchive-3.3.2
 ./configure --prefix=/usr/local \
 	    --disable-static &&
 make
 EOF
-cd ~/.boost_install/libarchive-3.3.2
+cd libarchive-3.3.2
 make install -j2
-cd ~/.boost_install
 #     ________  ___      __           _____         ____          _____
 #    / ____/  |/  /___ _/ /_____     |__  /        / __ \        / ___/
 #   / /   / /|_/ / __ `/ //_/ _ \     /_ <        / /_/ /       / __ \ 
 #  / /___/ /  / / /_/ / ,< /  __/   ___/ /  _     \__, /  _    / /_/ / 
 #  \____/_/  /_/\__,_/_/|_|\___/   /____/  (_)   /____/  (_)   \____/  
-#                                                                       
+#
+cd ..                                                                 
 su $SUDO_USER << 'EOF'
-cd ~/.boost_install/
 wget https://cmake.org/files/v3.9/cmake-3.9.6.tar.gz
 tar -xvzf cmake-3.9.6.tar.gz
-cd ~/.boost_install/cmake-3.9.6
+cd cmake-3.9.6
 sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
 ./bootstrap --prefix=/usr/local            \
 	    --system-libs                  \
@@ -94,20 +93,19 @@ sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
 	    --docdir=/usr/local/share/doc/cmake-3.9.6 &&
 make
 EOF
-cd ~/.boost_install/cmake-3.9.6
+cd cmake-3.9.6
 make install -j2
-cd ~/.boost_install
 #      ____                   __     ___        _____ ______        ___
 #     / __ )____  ____  _____/ /_   <  /       / ___// ____/       <  /
 #    / __  / __ \/ __ \/ ___/ __/   / /       / __ \/___ \         / / 
 #   / /_/ / /_/ / /_/ (__  ) /_    / /  _    / /_/ /___/ /  _     / /  
 #  /_____/\____/\____/____/\__/   /_/  (_)   \____/_____/  (_)   /_/   
-#                                                                        
+#
+cd ..                                                                    
 su $SUDO_USER << 'EOF'
-cd ~/.boost_install
 wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
 tar -xvzf boost_1_65_1.tar.gz
-cd ~/.boost_install/boost_1_65_1
+cd boost_1_65_1
 sed -e '/using python/ s@;@: /usr/include/python${PYTHON_VERSION/3*/${PYTHON_VERSION}m} ;@' \
     -i bootstrap.sh
 ./bootstrap.sh --prefix=/usr/local            \
@@ -118,7 +116,7 @@ sed -e '/using python/ s@;@: /usr/include/python${PYTHON_VERSION/3*/${PYTHON_VER
 echo "using python : 3.5 : /usr/bin/python3.5 : /usr/include/python3.5m : /usr/lib ;" >> project-config.jam
 ./b2 stage threading=multi link=shared -j 2
 EOF
-cd ~/.boost_install/boost_1_65_1
+cd boost_1_65_1
 ./b2 install threading=multi link=shared -j 2
 #      ____                         ___        ____          ____ 
 #     / __/  ___________  __  __   <  /       / __ \        / __ \
@@ -126,19 +124,19 @@ cd ~/.boost_install/boost_1_65_1
 #   / __/>  </ /__/ /_/ / /_/ /   / /  _    / /_/ /  _    / /_/ / 
 #  /_/ /_/|_|\___/ .___/\__, /   /_/  (_)   \____/  (_)   \____/  
 #               /_/    /____/                                     
-cd ~
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/fxcpy/cpp/lib" >> /etc/environment
-ldconfig
+cd ../..
+# Clean install files
+rm -rf .boost_install
+P=$PWD
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${P}/cpp/lib" >> /etc/environment 
 su $SUDO_USER << 'EOF'
 # Compile Forexconnect API cpp code and install fxcpy
-cd ~/fxcpy/cpp && mkdir build && cd ~/fxcpy/cpp/build
+cd cpp && mkdir build && cd build
 cmake ..
 EOF
-cd ~/fxcpy/cpp/build
+cd cpp/build
 make install -j2
-cd ~/fxcpy
+cd ../..
 pip3 install . --no-cache-dir
 ldconfig
-# Clean install files
-rm -rf ~/.boost_install
 exit 0
