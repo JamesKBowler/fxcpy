d166e24109f5ea4a8faa4a626038b1889b23cd52
James Bowler <james.bowler@datacentauri.com>
2018-06-04 11:59:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -128,7 +128,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandas'],  # Optional
+    install_requires=['pandas', 'eventfd'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
