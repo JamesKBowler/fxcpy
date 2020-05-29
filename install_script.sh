91a40e63c22d6772f634d79b1ced2a0b697e4ee6
James Bowler <jbowler1983@gmail.com>
2020-05-29 23:02:38 +0100

    Updating to Python 3.6

--- install_script.sh
+++ install_script.sh
@@ -33,18 +33,16 @@ apt-get -y install python3-dev
 apt-get -y install libtool m4 automake autogen make g++
 apt-get -y install libcurl4-openssl-dev
 apt-get -y install zlib1g-dev
-pip3 install ipython
-su $SUDO_USER << 'EOF'
+su "$SUDO_USER" << 'EOF'
 mkdir .boost_install
 EOF
-pwd
 #      ___ __                   ___        _______          ___
 #     / (_) /_  __  ___   __   <  /       <  / __ \        <  /
 #    / / / __ \/ / / / | / /   / /        / / /_/ /        / / 
 #   / / / /_/ / /_/ /| |/ /   / /  _     / /\__, /  _     / /  
 #  /_/_/_.___/\__,_/ |___/   /_/  (_)   /_//____/  (_)   /_/   
 #                                                              
-su $SUDO_USER << 'EOF'
+su "$SUDO_USER" << 'EOF'
 cd .boost_install
 wget https://github.com/libuv/libuv/archive/v1.19.1/libuv-1.19.1.tar.gz
 tar -xvzf libuv-1.19.1.tar.gz > /dev/null
@@ -54,7 +52,7 @@ sh autogen.sh  &&
             --disable-static &&
 make -s
 EOF
-cd .boost_install/libuv-1.19.1
+cd .boost_install/libuv-1.19.1 || exit
 make install -j2
 #      ___ __                    __    _               _____         _____         ___ 
 #     / (_) /_  ____ ___________/ /_  (_)   _____     |__  /        |__  /        |__ \
@@ -63,7 +61,7 @@ make install -j2
 #  /_/_/_.___/\__,_/_/   \___/_/ /_/_/ |___/\___/   /____/  (_)   /____/  (_)   /____/ 
 #                     
 cd ..                           
-su $SUDO_USER << 'EOF'
+su "$SUDO_USER" << 'EOF'
 wget http://www.libarchive.org/downloads/libarchive-3.3.2.tar.gz
 tar -xvzf libarchive-3.3.2.tar.gz > /dev/null
 cd libarchive-3.3.2
@@ -71,7 +69,7 @@ cd libarchive-3.3.2
 	    --disable-static &&
 make -s
 EOF
-cd libarchive-3.3.2
+cd libarchive-3.3.2 || exit
 make install -j2
 #     ________  ___      __           _____         ____          _____
 #    / ____/  |/  /___ _/ /_____     |__  /        / __ \        / ___/
@@ -80,7 +78,7 @@ make install -j2
 #  \____/_/  /_/\__,_/_/|_|\___/   /____/  (_)   /____/  (_)   \____/  
 #
 cd ..                                                                 
-su $SUDO_USER << 'EOF'
+su "$SUDO_USER" << 'EOF'
 wget https://cmake.org/files/v3.9/cmake-3.9.6.tar.gz
 tar -xvzf cmake-3.9.6.tar.gz > /dev/null
 cd cmake-3.9.6
@@ -93,7 +91,7 @@ sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
 	    --docdir=/usr/local/share/doc/cmake-3.9.6 &&
 make
 EOF
-cd cmake-3.9.6
+cd cmake-3.9.6 || exit
 make install -j2
 #      ____                   __     ___        _____ ______        ___
 #     / __ )____  ____  _____/ /_   <  /       / ___// ____/       <  /
@@ -102,7 +100,7 @@ make install -j2
 #  /_____/\____/\____/____/\__/   /_/  (_)   \____/_____/  (_)   /_/   
 #
 cd ..                                                                    
-su $SUDO_USER << 'EOF'
+su "$SUDO_USER" << 'EOF'
 wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
 tar -xvzf boost_1_65_1.tar.gz > /dev/null
 cd boost_1_65_1
@@ -110,13 +108,13 @@ sed -e '/using python/ s@;@: /usr/include/python${PYTHON_VERSION/3*/${PYTHON_VER
     -i bootstrap.sh
 ./bootstrap.sh --prefix=/usr/local            \
         --with-python=/usr/bin/python3        \
-        --with-python-version=3.5             \
-        --with-python-root=/usr/lib/python3.5 \
+        --with-python-version=3.6             \
+        --with-python-root=/usr/lib/python3.6 \
         --with-libraries=python
-echo "using python : 3.5 : /usr/bin/python3.5 : /usr/include/python3.5m : /usr/lib ;" >> project-config.jam
+echo "using python : 3.6 : /usr/bin/python3.6 : /usr/include/python3.6m : /usr/lib ;" >> project-config.jam
 ./b2 stage threading=multi link=shared -j2 -d0
 EOF
-cd boost_1_65_1
+cd boost_1_65_1 || exit
 ./b2 install threading=multi link=shared -j2 -d0
 #      ____                         ___        ____          ____ 
 #     / __/  ___________  __  __   <  /       / __ \        / __ \
@@ -129,12 +127,12 @@ cd ../..
 rm -rf .boost_install
 P=$PWD
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${P}/cpp/lib" >> /etc/environment 
-su $SUDO_USER << 'EOF'
+su "$SUDO_USER" << 'EOF'
 # Compile Forexconnect API cpp code and install fxcpy
 cd cpp && mkdir build && cd build
 cmake ..
 EOF
-cd cpp/build
+cd cpp/build || exit
 make install -j2
 cd ../..
 pip3 install . --no-cache-dir
